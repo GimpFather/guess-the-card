816cb398b6494cfada1041f8dc4f6dd0365cf089
spthiel <sp4rklingskull@gmail.com>
2025-06-27 20:35:38 +0200

    Add card name suggestions so the game isn't a spelling bee.

--- src/components/SandboxMode/GuessingForm.tsx
+++ src/components/SandboxMode/GuessingForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, TextField } from "@mui/material";
+import {Autocomplete, Button, Stack, TextField, type FilterOptionsState} from "@mui/material";
 import { useForm } from "react-hook-form";
 import type { FieldValues } from "react-hook-form";
 import {
@@ -11,6 +11,7 @@ import { FormattedMessage } from "react-intl";
 import { toast } from "react-toastify";
 import { markCardAsGuessed } from "../../localStorage/mechanics";
 import { motion } from "framer-motion";
+import {CARDNAMES} from "../../coreFiles/cardnames.ts";
 
 interface FormData extends FieldValues {
   "Card Name": string;
@@ -46,6 +47,19 @@ const GuessingForm = ({
       reset();
     }
   };
+  
+  const normalizeValue = (value: string): string => {
+    return value.toLowerCase().replace(/[^a-z ]/g, '')
+  }
+  
+  const filterOptions = (options: string[], state: FilterOptionsState<string>) => {
+    console.log(options, state);
+    const input = normalizeValue(state.inputValue);
+    
+    return options
+        .filter(option => normalizeValue(option).includes(input))
+        .slice(0, 100);
+  }
 
   return (
     <Stack
@@ -59,11 +73,27 @@ const GuessingForm = ({
       <form onSubmit={handleSubmit(onSubmit)}>
         <Stack gap={2}>
           <Stack direction="row" gap={2}>
-            <TextField
-              label={<FormattedMessage id="GUESSING_FORM.LABEL.CARD_NAME" />}
-              {...register("Card Name")}
-              disabled={guessed}
+            <Autocomplete
+              disablePortal
+              disableClearable
+              options={CARDNAMES}
+              filterOptions={filterOptions}
               fullWidth
+              sx={{width: 220}}
+              renderInput={(params) => <TextField
+                  {...params}
+                  label={<FormattedMessage id="GUESSING_FORM.LABEL.CARD_NAME"/>}
+                  {...register("Card Name")}
+                  fullWidth
+                  disabled={guessed}
+              />}
+              renderOption={(props, option) => {
+                return (
+                    <span {...props} style={{ color: '#F9F9F9' }}>
+                      {option}
+                    </span>
+                );
+              }}
             />
             <Button
               variant="contained"
